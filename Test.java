00e13c40f3efa4e274a6de317dcc61306466f0e0
prashantpondkule123 <prashantpondkule123@gmail.com>
2025-10-05 14:26:52 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -9,24 +9,14 @@ public class Test {
 
 	public static void main(String[] args) {
 
-		HashMap<String, Integer> map = new HashMap<String, Integer>();
-		map.put("prashant", 36);
-		map.put("rushi", 52);
-		map.put("adhira", 68);
-		map.put("bd", 25);
-		map.put("sachin", 52);
-		
-		Set<String> set = map.keySet();
-		ArrayList<String> list = new ArrayList<String>(set);
-		Collections.sort(list);
-		
-		LinkedHashMap<String, Integer> sortedMap = new LinkedHashMap<String, Integer>();
-		for (String k : list) {
-			sortedMap.put(k, map.get(k));
-		}
-		System.out.println(sortedMap);
+		System.setProperty("webdriver.chrome.driver","D:/Java Data/Chrome Browser1/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://chromedriver.chromium.org/downloads");
+
 
 	}
 
 }
+
 
